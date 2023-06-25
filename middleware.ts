39242a4efb35e98b0ff33664c01368eb0e4f99e3
jacsonikowy <michal6345@wp.pl>
2023-06-25 14:01:45 +0200

    feat: add protect from routes

--- middleware.ts
+++ middleware.ts
@@ -3,5 +3,5 @@ import withAuth from "next-auth/middleware";
 export default withAuth(async () => {});
 
 export const config = {
-  matcher: ["/dashboard"],
+  matcher: ["/messages", "/friends", "/settings"],
 };
