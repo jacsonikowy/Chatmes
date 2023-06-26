ef910dce9c09a7ed029adc4fdc3cf803124b2214
jacsonikowy <michal6345@wp.pl>
2023-06-26 12:17:14 +0200

    fix: add validation

--- app/api/message/send/route.ts
+++ app/api/message/send/route.ts
@@ -3,12 +3,14 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/authOptions";
 import { pusherServer } from "@/lib/pusher";
 import { toPusherKey } from "@/lib/utils";
+import { messageValidator } from "@/lib/validations/send-message";
+import { z } from "zod";
+import { zhCN } from "date-fns/esm/locale";
 
 export const POST = async (req: Request) => {
   try {
     const body = await req.json();
     const { text, chatId } = body;
-
     const timestamp = Date.now();
 
     const session = await getServerSession(authOptions);
@@ -24,17 +26,24 @@ export const POST = async (req: Request) => {
       text: text,
     };
 
+    const validatedMessage = messageValidator.parse(message);
+
     db.zadd(`chat:${chatId}:messages`, {
       score: timestamp,
-      member: JSON.stringify(message),
+      member: JSON.stringify(validatedMessage),
     });
 
-    pusherServer.trigger(toPusherKey(`chat:${chatId}:messages`), "messages", {
-      message,
+    pusherServer.trigger(toPusherKey(`chat:${chatId}:messages`), `messages`, {
+      message: validatedMessage,
     });
 
     return new Response("OK");
   } catch (error) {
+    console.log(error);
+    if (error instanceof z.ZodError) {
+      console.log("asd");
+      return new Response(error.message, { status: 500 });
+    }
     if (error instanceof Error) {
       return new Response(error.message, { status: 500 });
     }
